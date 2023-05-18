f36de37f04bb75adb2d78f29ff80702d182d1208
username <email address>
2023-05-18 17:28:40 +0530

    okie

--- js2class/employee/emp.js
+++ js2class/employee/emp.js
@@ -1,21 +1,23 @@
-<html>
+let employees = [
+    { id: 101, name: 'Rahul Gandhi', salary: 45000 },
+    { id: 102, name: 'Sonai Gandhi', salary: 55000 },
+    { id: 103, name: 'Priyanka Gandhi', salary: 65000 },
+    { id: 104, name: 'Modi JI', salary: 75000 },
+    { id: 101, name: 'Rahul Gandhi', salary: 45000 },
+    { id: 102, name: 'Sonai Gandhi', salary: 55000 },
+    { id: 103, name: 'Priyanka Gandhi', salary: 65000 },
+    { id: 104, name: 'Modi JI', salary: 75000 }
+]
 
-<head>
-    <script src="./data.js" type="text/javascript"></script>
-</head>
-
-<body onload="display_Data()">
-    <h1>Employee Data</h1>
-    <table border="2">
-        <thead>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Salary</th>
-        </thead>
-        <tbody id="tbody_Data">
-            
-        </tbody>
-    </table>
-</body>
-
-</html>
+function display_Data() {
+    let rows = ""
+    for (employee of employees) {
+        rows = rows + `<tr>
+            <td>${employee.id}</td >
+            <td>${employee.name}</td >
+            <td>${employee.salary}</td >
+        </tr >`
+    }
+    //document.getElementById('abc').innerHTML = "GM"
+    document.getElementById('abc').innerHTML = rows
+}
---
+++ js2class/events/data.js
@@ -0,0 +1,4 @@
+function display_Message() {
+    //alert("Hello,GM")
+    document.write("good to go with this")
+}
